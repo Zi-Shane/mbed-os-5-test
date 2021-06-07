6dbbf084456ae84f2838dbcd72eddeb452b1374d
tshuang <tshuang@smail.nchu.edu.tw>
2021-06-07 16:53:57 +0800

    OK: stop thread

--- main.cpp
+++ main.cpp
@@ -3,19 +3,24 @@
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 Thread thread;
- 
-void led2_thread() {
-    while (true) {
-        led2 = !led2;
-        ThisThread::sleep_for(2000);
+// volatile bool running = true;
+
+// Blink function toggles the led in a long running loop
+void blink(DigitalOut *led) {
+    for(int i = 0;i < 3;i++){
+        *led = !*led;
+        ThisThread::sleep_for(3000);
     }
 }
  
 int main() {
-    thread.start(led2_thread);
+    thread.start(callback(blink, &led1));
+
+    // running = false;
+    thread.join();
     
     while (true) {
-        led1 = !led1;
+        led2 = !led2;
         ThisThread::sleep_for(1000);
     }
 }
