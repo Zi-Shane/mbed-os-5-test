70e9113bfd8faab7a5ec039e8f347f91f7956a1b
tshuang <tshuang@smail.nchu.edu.tw>
2021-06-07 16:32:47 +0800

    OK: blinky use defferent thread

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@ Thread thread;
 void led2_thread() {
     while (true) {
         led2 = !led2;
-        ThisThread::sleep_for(2);
+        ThisThread::sleep_for(2000);
     }
 }
  
 int main() {
-    // thread.start(led2_thread);
+    thread.start(led2_thread);
     
     while (true) {
         led1 = !led1;
-        ThisThread::sleep_for(1);
+        ThisThread::sleep_for(1000);
     }
 }
