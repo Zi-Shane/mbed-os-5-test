e8ba08f7137551c2da51d0418ca2999f56c2069c
tshuang <tshuang@smail.nchu.edu.tw>
2021-06-09 16:34:04 +0800

    TCP Socket as server (but ESP8266 not support)
    From:
    https://os.mbed.com/users/JohnnyK/code/TCP_EchoServer/
    Error:
    https://forums.mbed.com/t/tcp-server-with-esp8266/13326

--- main.cpp
+++ main.cpp
@@ -1,101 +1,126 @@
+#include <algorithm>
 #include "mbed.h"
+#include "mbed_stats.h"
+#include "TCPSocket.h"
+#include "mbed_events.h"
 
-// Network interface
-NetworkInterface *net;
+DigitalOut led1(LED1);
 
-// Socket demo
+#define MBED_HEAP_STATS_ENABLED 1
+
+#define PORT 4063
+#define BUFFSIZE 10
+
 int main() {
-    int remaining;
-    int rcount;
-    char *p;
-    char *buffer = new char[256];
-    nsapi_size_or_error_t result;
-
-    // Bring up the ethernet interface
-    printf("Mbed OS Socket example\n");
-
 #ifdef MBED_MAJOR_VERSION
-    printf("Mbed OS version: %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+    printf("Mbed OS version %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 #endif
 
-    net = NetworkInterface::get_default_instance();
+    int rc = 0;
 
-    if (!net) {
-        printf("Error! No network inteface found.\n");
-        return 0;
+    printf("Start Connection ... \r\n");
+
+    NetworkInterface *network_interface = NetworkInterface::get_default_instance();
+    if (NULL == network_interface) {
+        printf("NULL network interface! Exiting application....\r\n");
+        return 1;
     }
 
-    result = net->connect();
-    if (result != 0) {
-        printf("Error! net->connect() returned: %d\n", result);
-        return result;
+    printf("\n\rUsing WiFi \r\n");
+    printf("\n\rConnecting to WiFi..\r\n");
+    rc = network_interface->connect();
+    if(rc == 0) {
+        printf("\n\rConnected to Network successfully\r\n");
+    } else {
+        printf("\n\rConnection to Network Failed %d! Exiting application....\r\n", rc);
+        return 1;
     }
 
-    // Show the network address
-    SocketAddress a;
-    net->get_ip_address(&a);
-    printf("IP address: %s\n", a.get_ip_address() ? a.get_ip_address() : "None");
-    net->get_netmask(&a);
-    printf("Netmask: %s\n", a.get_ip_address() ? a.get_ip_address() : "None");
-    net->get_gateway(&a);
-    printf("Gateway: %s\n", a.get_ip_address() ? a.get_ip_address() : "None");
+    TCPSocket server;
+    TCPSocket* client;
+    SocketAddress clientAddress;
+    
+    int err;
+    char   *in_buffer;
+    char   *out_buffer;
 
-    // Open a socket on the network interface, and create a TCP connection to ifconfig.io
-    TCPSocket socket;
-    // Send a simple http request
-    char sbuffer[] = "GET / HTTP/1.1\r\nHost: ifconfig.io\r\nConnection: close\r\n\r\n";
-    nsapi_size_t size = strlen(sbuffer);
+    SocketAddress ip; 
+    network_interface->get_ip_address(&ip);
+    const char *p_ip = ip.get_ip_address();
+    printf("IP address: %s and Port: %d\n", p_ip ? p_ip : "None", PORT);
 
-    result = socket.open(net);
-    if (result != 0) {
-        printf("Error! socket.open() returned: %d\n", result);
-    }
+    // SocketAddress mask;
+    // network_interface->get_netmask(&mask);
+    // const char *p_mask = mask.get_ip_address();
+    // printf("Netmask: %s\n", p_mask ? p_mask : "None");
 
-    result = net->gethostbyname("ifconfig.io", &a);
-    if (result != 0) {
-        printf("Error! Could not resolve host ifconfig.io. Result: %d\n", result);
-    }
-    a.set_port(80);
-    result = socket.connect(a);
-    if (result != 0) {
-        printf("Error! socket.connect() returned: %d\n", result);
-        goto DISCONNECT;
-    }
+    // SocketAddress gateway;
+    // network_interface->get_gateway(&gateway);
+    // const char *p_gateway = gateway.get_ip_address();
+    // printf("Gateway: %s\n", p_gateway ? p_gateway : "None");
 
-    // Loop until whole request sent
-    while(size) {
-        result = socket.send(sbuffer+result, size);
-        if (result < 0) {
-            printf("Error! socket.send() returned: %d\n", result);
-            goto DISCONNECT;
+    if (ip) {
+        err = server.open(network_interface);
+        if (err != 0) {
+            printf("Error! open() returned: %d\r\n", err);
+        } else {
+            printf("open() successfully\n");
         }
-        size -= result;
-        printf("sent %d [%.*s]\n", result, strstr(sbuffer, "\r\n")-sbuffer, sbuffer);
+
+        server.bind(PORT);
+        if (err != 0) {
+            printf("Error! bind() returned: %d\r\n", err);
+        } else {
+            printf("bind() successfully\n");
+        }
+        server.listen(1);
+        if (err != 0) {
+            printf("Error! listen() returned: %d\r\n", err);
+        } else {
+            printf("listen() successfully\n");
+        }
+
+        while (1) {
+            // printf("Server bound and listening\n");
+            client = server.accept(&err);
+            if (err != NULL) {
+                printf("Error! listen() returned: %d\r\n", err);
+            } else {
+                printf("accept() successfully\n");
+            }
+            // client->getpeername(&clientAddress);
+            // const char *p_clientAddress = clientAddress.get_ip_address();
+            // printf("Client connected from IP address: %s\n", p_clientAddress ? p_clientAddress : "None");
+
+            bool b = true;
+            while(b) {
+                in_buffer = new char[BUFFSIZE];
+                int n = client->recv(in_buffer, BUFFSIZE);
+                if (n == 0) {
+                    printf("Client disconnected\n");
+                    b = false;
+                } else {
+                    in_buffer[n] = '\0';
+                    printf("Received message from Client :'%s'\n", in_buffer);
+                    out_buffer = new char[BUFFSIZE];
+                    n = sprintf(out_buffer,"Echo - %s", in_buffer);
+
+                    printf("Sending echo to client\n");
+                    client->send(out_buffer, n);
+                    delete []out_buffer;
+                }
+            }
+            client->close();
+        }
+    } else {
+        printf("No IP\n");
+        network_interface->disconnect();
+        printf("Program end\n");
     }
+  
+    // while (true){
+    //     led1 = !led1;
+    //     ThisThread::sleep_for(1000);
+    // }
 
-    // Receieve an HTTP response and print out the response line
-    remaining = 256;
-    rcount = 0;
-    p = buffer;
-    while (remaining > 0 && 0 < (result = socket.recv(p, remaining))) {
-        p += result;
-        rcount += result;
-        remaining -= result;
-    }
-    if (result < 0) {
-        printf("Error! socket.recv() returned: %d\n", result);
-        goto DISCONNECT;
-    }
-	// the HTTP response code
-    printf("recv %d [%.*s]\n", rcount, strstr(buffer, "\r\n")-buffer, buffer);
-
-    delete[] buffer;
-
-DISCONNECT:
-    // Close the socket to return its memory and bring down the network interface
-    socket.close();
-
-    // Bring down the ethernet interface
-    net->disconnect();
-    printf("Done\n");
 }
